b9cfa617a1b379defe4cb996c037898caf7d283d
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-11 16:11:28 +0100

    creating unit test to fail

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,10 @@
+require "docking_station"
 #RSpec.describe DockingStation do   
 describe DockingStation do
+    # it "should release bike" do
+    #     expect(respond('release_bike')).to eq  'Bike is released'
+    # end
+
+    it {is_expected.to release_bike}
+
 end
