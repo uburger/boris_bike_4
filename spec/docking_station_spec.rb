37775781b7d456caf60f623b036cd40213a1c033
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-10-11 16:29:08 +0100

    passing unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,15 @@
 require "docking_station"
 #RSpec.describe DockingStation do   
 describe DockingStation do
+    before do 
+        @docking_station = DockingStation.new
+    end
     # it "should release bike" do
     #     expect(respond('release_bike')).to eq  'Bike is released'
     # end
 
-    it {is_expected.to release_bike}
+    #it {is_expected.to @docking_station.release_bike}
+    it {should @docking_station.release_bike}
+
 
 end
