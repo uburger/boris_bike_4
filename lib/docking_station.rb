30be1b9cfea0a3f2dca83da7a6628e98dedd5801
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-13 15:38:09 +0100

    Started adding a test for if there are no bikes user gets an error message

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,23 +1,24 @@
 require_relative 'bike'
 
 class DockingStation
-    attr_reader :a_bike, :bike_avail
+    attr_reader :a_bike, :bikes_docked
 
-    def initialize(a_bike, bike_avail)
+    def initialize
         @a_bike = a_bike
-        @bike_avail = bike_avail
+        @bikes_docked = []
     end
 
     def release_bike
-        @a_bike = Bike.new
+        @a_bike = @bikes_docked.last
+
     end 
 
-    def dock_bike(returned_bike)
-        returned_bike = @a_bike
+    def dock_bike
         return "Bike #{@a_bike} docked"
     end
 
     def has_bike?
-        if @bike_avail = true
-end 
+        true
+    end
+end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,36 +4,41 @@ describe DockingStation do
 
     it 'releases a bike' do
     
-      station = DockingStation.new('a_bike')
+      station = DockingStation.new
   
       expect(station).to respond_to :release_bike
     end
 
     it 'releases a  working bike' do
-    
-      station = subject.release_bike
+      
+      #this is an exception as it creates a bike object via docking station
+      new_bike = subject.release_bike
   
-      expect(station).to eq true
+      expect(new_bike.working?).to eq true
     end
 
 
 
     it 'docks the bike' do
       
-      station = DockingStation.new('bike1', true)
+      station = DockingStation.new
 
-      station.dock_bike('a_bike')
-
-      expect(station.dock_bike('a_bike', true)).to eq "Bike #{station.a_bike} docked"
+      expect(station.dock_bike).to eq "Bike #{station.a_bike} docked"
 
     end
 
     it 'checks if any bikes docked' do
-      station = DockingStation.new('bike1', true)
+      station = DockingStation.new
 
-      station.has_bike?('a_bike')
+      station.release_bike
 
-      expect(station.has_bike?('a_bike', true)).to eq "Bike #{station.a_bike} available"
+      expect(station.has_bike?).to eq true
+    end
+
+    it 'checks if no bikes available' do
+      station = DockingStation.new
+
+      expect(station.has_bike?).to eq false
     end
 
 end
