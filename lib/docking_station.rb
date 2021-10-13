3b70ba12778ceae8b5a4437ae1ac8edbe2a7333f
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-13 14:15:13 +0100

    Added tests for whether the docking station has a bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,22 @@
 class DockingStation
+    attr_reader :a_bike, :bike_avail
+
+    def initialize(a_bike, bike_avail)
+        @a_bike = a_bike
+        @bike_avail = bike_avail
+    end
+
     def release_bike
-        bike = Bike.new
-        return bike.working?
+        @a_bike = Bike.new
+        return @a_bike.working?
     end 
+
+    def dock_bike(returned_bike)
+        returned_bike = @a_bike
+        return "Bike #{@a_bike} docked"
+    end
+
+    def has_bike?
+        if @bike_avail = true
 end 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,30 @@
-require "docking_station"
+require 'docking_station'
 
 describe DockingStation do
 
-    it 'returns empty'do
+    it 'releases a bike' do
     
-      docking_station = DockingStation.new 
+      station = DockingStation.new('a_bike')
   
-      expect(docking_station.release_bike).to eq true
+      expect(station.release_bike).to eq true
+    end
+
+    it 'docks the bike' do
+      
+      station = DockingStation.new('bike1', true)
+
+      station.dock_bike('a_bike')
+
+      expect(station.dock_bike('a_bike', true)).to eq "Bike #{station.a_bike} docked"
+
+    end
+
+    it 'checks if any bikes docked' do
+      station = DockingStation.new('bike1', true)
+
+      station.has_bike?('a_bike')
+
+      expect(station.has_bike?('a_bike', true)).to eq "Bike #{station.a_bike} available"
     end
 
 end
