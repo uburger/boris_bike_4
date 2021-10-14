019fd234f94c5ab5882dbd23404ff55691f58d11
michael steinacher <steinacherm@hotmail.co.uk>
2021-10-14 09:36:31 +0100

    Corrected the docking_station_spec tests to prevent infinite bikes being created and improved the dock, has_bike and release_bike methods in the docking_station.rb file.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,24 +1,27 @@
 require_relative 'bike'
 
 class DockingStation
-    attr_reader :a_bike, :bikes_docked
+    attr_reader :bikes_docked
 
     def initialize
-        @a_bike = a_bike
         @bikes_docked = []
     end
 
     def release_bike
         @a_bike = @bikes_docked.last
-
     end 
 
-    def dock_bike
-        return "Bike #{@a_bike} docked"
+    def dock(bike)
+        @bikes_docked << bike
+        return bike
     end
 
     def has_bike?
-        true
+        if bikes_docked.empty?
+            false
+        else
+            true
+        end
     end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,34 +9,34 @@ describe DockingStation do
       expect(station).to respond_to :release_bike
     end
 
-    it 'releases a  working bike' do
+    it 'releases a working bike' do
       
-      #this is an exception as it creates a bike object via docking station
-      new_bike = subject.release_bike
+      bike = Bike.new
   
-      expect(new_bike.working?).to eq true
+      expect(bike.working?).to eq true
     end
-
-
 
     it 'docks the bike' do
       
       station = DockingStation.new
+      bike = Bike.new
 
-      expect(station.dock_bike).to eq "Bike #{station.a_bike} docked"
+      expect(station.dock(bike)).to eq bike
 
     end
 
     it 'checks if any bikes docked' do
       station = DockingStation.new
-
-      station.release_bike
+      bike = Bike.new
+      
+      station.dock(bike)
 
       expect(station.has_bike?).to eq true
     end
 
     it 'checks if no bikes available' do
       station = DockingStation.new
+      bike = Bike.new
 
       expect(station.has_bike?).to eq false
     end
