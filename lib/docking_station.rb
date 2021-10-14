c8dc290db5e86f2224a97df9de0cb24bce2da8b7
uburger <ursburger@gmail.com>
2021-10-14 15:45:37 +0200

    Finished 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,24 +4,20 @@ class DockingStation
     attr_reader :bikes_docked
 
     def initialize
-        @bikes_docked = []
+      
     end
 
     def release_bike
-        @a_bike = @bikes_docked.last
+        fail "No bike available" unless @bikes_docked 
+        return @bikes_docked  
+        
     end 
 
     def dock(bike)
-        @bikes_docked << bike
-        return bike
+        @bikes_docked = bike
+        
     end
 
-    def has_bike?
-        if bikes_docked.empty?
-            false
-        else
-            true
-        end
-    end
+    
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,7 @@ describe DockingStation do
     it 'releases a bike' do
       bike = Bike.new
       station = DockingStation.new
+
       station.dock(bike)
       expect(station.release_bike).to eq (bike)
     end
@@ -28,17 +29,15 @@ describe DockingStation do
     it 'checks if any bikes docked' do
       station = DockingStation.new
       bike = Bike.new
-      
-      station.dock(bike)
 
-      expect(station.has_bike?).to eq true
+      station.dock(bike)
+      expect(station.release_bike).to eq (bike)
     end
 
     it 'checks if no bikes available' do
       station = DockingStation.new
-      bike = Bike.new
 
-      expect(station.has_bike?).to eq false
+      expect { station.release_bike }.to raise_error "No bike available" 
     end
 
 end
